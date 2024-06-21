eedbe3d421409c96056f3b04c46788beb709e42e
dlotterman <dlotterman@users.noreply.github.com>
2024-06-21 20:53:39 +0000

    This is HPC

--- bin/metal_nvme_manage.sh
+++ bin/metal_nvme_manage.sh
@@ -7,20 +7,32 @@ else
 		exit 0
 fi
 
-NVME_DRIVES=$(nvme list-subsys | grep pcie | awk '{print"/dev/"$2}')
+# We grep out TB here for the systems that have 256GB boot NVMes
+NVME_DRIVES=$(nvme list-subsys | grep pci | awk '{print"/dev/"$2}')
 for DRIVE in $NVME_DRIVES; do
 	logger "working on nvme $DRIVE"
 	CONTRLID=$(nvme id-ctrl $DRIVE | grep cntlid | awk '{print$NF}')
-	TOTAL_NAMESPACES=$(nvme id-ctrl /dev/nvme0 | grep nn | awk '{print$NF}')
+	TOTAL_NAMESPACES=$(nvme id-ctrl $DRIVE | grep nn | awk '{print$NF}')
 	if [[ "$TOTAL_NAMESPACES" == 0 ]]; then
-		logger "cannot operate on drives with no namespace support"
-		exit 0
+		logger "cannot operate on drives with no namespace support, drive $DRIVE"
+		continue
+	elif [[ "$TOTAL_NAMESPACES" == 1 ]]; then
+		logger "cannot operate on drives with no namespace support, drive $DRIVE"
+		continue
 	fi
 	if test -z "$TOTAL_NAMESPACES"; then
-		logger "cannot operate on drives with no namespace support"
-		exit 0
+		logger "cannot operate on drives with no namespace support, drive $DRIVE"
+		continue
 	fi
-	HALF_NAMESPACES=$(( $TOTAL_NAMESPACES / 2 ))
+	TOTAL_SPACE=$(nvme id-ctrl $DRIVE | grep tnvmcap | awk '{print$NF}')
+	if [[ "$TOTAL_SPACE" -lt 3800000000000 ]]; then
+		logger "drive $DRIVE to small to be a Metal data drive"
+		continue
+	fi
+	# Sigh, you can get varying features of NS
+	#HALF_NAMESPACES=$(( $TOTAL_NAMESPACES / 2 ))
+	# Magic numbering this
+	HALF_NAMESPACES=16
 	for NS in $(seq 1 $TOTAL_NAMESPACES); do
 		nvme detach-ns $DRIVE -n $NS -c $CONTRLID > /dev/null 2>&1
 		sleep .1
@@ -28,7 +40,6 @@ for DRIVE in $NVME_DRIVES; do
 		nvme delete-ns $DRIVE -n $NS > /dev/null 2>&1
 	done
 	nvme ns-rescan $DRIVE
-	TOTAL_SPACE=$(nvme id-ctrl $DRIVE | grep tnvmcap | awk '{print$NF}')
 	TOTAL_BS_SPACE=$(echo "$TOTAL_SPACE/4096" | bc)
 	SPACE_PER_DRIVE=$(( $TOTAL_BS_SPACE / $HALF_NAMESPACES ))
 	#SPACE_PER_DRIVE_HEADROOM=$(echo "$SPACE_PER_DRIVE*.8" | bc)
@@ -42,4 +53,8 @@ for DRIVE in $NVME_DRIVES; do
 		nvme ns-rescan $DRIVE
 	done
 done
+partprobe
+sleep 1
+sync
+udevadm settle
 touch /opt/equinix/metal/tmp/metal_nvme_manage.lock
